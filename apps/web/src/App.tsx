50a0ce52bdb4265afe9de52a26927f4c12fc23af
ivatra <ivatra@yandex.ru>
2026-02-22 23:32:46 +0700

    chore (web): install and integrate mantine

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,19 @@
+import { Container, Title, Group, Button, Text } from "@mantine/core";
+
 function App() {
-  return <>Hello Izi</>;
+  return (
+    <Container py="xl">
+      <Title order={2}>Mantine + Vite + TS + IZI</Title>
+      <Text c="dimmed" mt="xs">
+        Все работает 🎉
+      </Text>
+
+      <Group mt="md">
+        <Button>Primary</Button>
+        <Button variant="light">Light</Button>
+      </Group>
+    </Container>
+  );
 }
 
 export default App;
