5bfc4e8b6928b74eafb40cddf657b75885670020
ivatra <ivatra@yandex.ru>
2026-02-23 03:22:25 +0700

    chore (web): add mantine/dates and dayjs packages

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "@mantine/core/styles.css";
+import "dayjs/locale/ru";
+import "@mantine/dates/styles.css";
 import { MantineProvider } from "@mantine/core";
 import App from "./App.tsx";
+import { DatesProvider } from "@mantine/dates";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="light">
-      <App />
+      <DatesProvider settings={{ locale: "ru" }}>
+        <App />
+      </DatesProvider>
     </MantineProvider>
   </StrictMode>,
 );
