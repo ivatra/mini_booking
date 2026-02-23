0fbf2a94afecd1f72561ccf9edeaa5d9fcd21f67
ivatra <ivatra@yandex.ru>
2026-02-23 15:50:42 +0700

    chore: (web): add eslint-prettier, eslint-plugin-import cfg, add new formating rules

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,10 +1,11 @@
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
 export default defineConfig([
   globalIgnores(["dist"]),
   {
@@ -19,5 +20,47 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "padding-line-between-statements": [
+        "warn",
+        { blankLine: "always", prev: "import", next: "*" },
+        { blankLine: "any", prev: "import", next: "import" },
+        { blankLine: "always", prev: "*", next: "return" },
+        { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+        {
+          blankLine: "any",
+          prev: ["const", "let", "var"],
+          next: ["const", "let", "var"],
+        },
+      ],
+
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+
+      "no-trailing-spaces": "warn",
+      "eol-last": ["warn", "always"],
+      "spaced-comment": ["warn", "always", { markers: ["/"] }],
+    },
   },
+  // Важно: отключает конфликтующие формат-правила ESLint с Prettier
+  eslintConfigPrettier,
 ]);
