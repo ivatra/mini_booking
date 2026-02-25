00952ba19a4efff18151b521cffd116d111d116b
ivatra <ivatra@yandex.ru>
2026-02-25 17:30:50 +0700

    feat (web header logo); on click go home page

--- apps/web/src/app/ui/layout/header/logo.tsx
+++ apps/web/src/app/ui/layout/header/logo.tsx
@@ -1,17 +1,23 @@
-import { Group, Title } from "@mantine/core";
+import { Box, Title } from "@mantine/core";
 import { IconBuilding } from "@tabler/icons-react";
+import { Link } from "react-router-dom";
+
+import s from "../layout.module.css";
 
 const iconColor =
   "light-dark(var(--mantine-color-blue-6), var(--mantine-color-blue-3))";
 
 const Logo = () => (
-  <Group gap="xs">
+  <Box
+    component={Link}
+    to="/"
+    className={s.headerLogo}>
     <IconBuilding
       size={22}
       style={{ color: iconColor }}
     />
     <Title order={4}>MiniBooking</Title>
-  </Group>
+  </Box>
 );
 
 export default Logo;
