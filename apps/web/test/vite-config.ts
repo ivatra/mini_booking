14e94683e15a7b7fa327002d536ed0bdbc5c7c37
ivatra <ivatra@yandex.ru>
2026-02-26 21:26:16 +0700

    test, feat (web): test-booking-store integration test

--- apps/web/test/vite-config.ts
+++ apps/web/test/vite-config.ts
@@ -5,7 +5,7 @@ export const USER_CONFIG: ViteUserConfig["test"] = {
   environment: "jsdom",
   globals: true,
   setupFiles: "test/setup.ts",
-  include: ["test/unit/**/*.t.{ts,tsx}", "test/integration/**/*.test.{ts,tsx}"],
+  include: ["test/unit/**/*.t.{ts,tsx}", "test/integration/**/*.t.{ts,tsx}"],
   coverage: {
     provider: "v8",
     reporter: ["text", "text-summary"],
